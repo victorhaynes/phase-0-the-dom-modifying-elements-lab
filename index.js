cf0332376eafcf55155fe940304fae464985185e
Victor Haynes <haynesvic@gmail.com>
2022-06-29 08:58:25 -0500

    commit tree 25ed4017663c97ffe6c8db00ed44f3ab4611e523

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+document.querySelector("main").remove();
+//entering an element would return the 1st eleemnt
+//#main would return an ID main
+//.main would return a class
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+
+newHeader.textContent = "YOUR-NAME is the champion"
